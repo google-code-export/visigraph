aa3cff0f1b405eafaba1e29653dd8ac4c10939b0
0x24a537r9 <0x24a537r9@8559fdbf-7309-dc73-cc61-87a2367b2425>
2010-06-24 17:42:06 +0000

    Added optional parameter for vertices to Empty Graph generator and fixed bug in distribute horizontally/vertically along the way. Build.

--- VisiGraph/src/edu/belmont/mth/visigraph/models/generators/EmptyGraphGenerator.java
+++ VisiGraph/src/edu/belmont/mth/visigraph/models/generators/EmptyGraphGenerator.java
@@ -3,15 +3,32 @@
  */
 package edu.belmont.mth.visigraph.models.generators;
 
+import java.awt.geom.*;
+import edu.belmont.mth.visigraph.models.*;
+
 /**
  * @author Cameron Behar
  * 
  */
 public class EmptyGraphGenerator extends AbstractGraphGenerator
 {
-	public boolean allowParameters()
+	public Graph generate(String args, boolean allowLoops, boolean allowDirectedEdges, boolean allowMultipleEdges, boolean allowCycles)
 	{
-		return false;
+		String[] params = args.split("\\s+");
+		int n = (params[0].trim().length() > 0 ? Integer.parseInt(params[0]) : 0);
+		
+		Graph ret = new Graph("Untitled " + getDescription(), allowLoops, allowDirectedEdges, allowMultipleEdges, allowCycles);
+		
+		double spacing = 150.0;
+		int rows = (int) Math.round(Math.sqrt(n));
+		int columns = (int) Math.ceil(n / (double) rows);
+		Point2D.Double location = new Point2D.Double((columns / 2.0) * -spacing, (columns / 2.0) * -spacing);
+		
+		for (int row = 0; row < rows; ++row)
+			for(int col = 0; (row < rows - 1 && col < columns) || (row == rows - 1 && col < (n % columns == 0 ? columns : n % columns)); ++col)
+				ret.vertexes.add(new Vertex(ret.nextVertexId(), location.x + spacing * col, location.y + spacing * row));
+		
+		return ret;
 	}
 	
 	public String getDescription()
