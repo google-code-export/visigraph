a91d6948ec0825ea8bdd442e177b26889c412762
0x24a537r9 <0x24a537r9@8559fdbf-7309-dc73-cc61-87a2367b2425>
2010-07-29 01:03:58 +0000

    Fixed Issue #34 (GraphDisplayView's getBounds() method should take edges and captions into account).  Build.

--- VisiGraph/src/edu/belmont/mth/visigraph/views/display/GraphDisplayView.java
+++ VisiGraph/src/edu/belmont/mth/visigraph/views/display/GraphDisplayView.java
@@ -32,25 +32,38 @@ public class GraphDisplayView
 	
 	public static Rectangle2D getBounds(Graph graph)
 	{
-		if(graph.vertexes.size() <= 0)
+		if (graph.vertexes.size() <= 0)
 			return null;
 		
 		double minX = Double.MAX_VALUE, minY = Double.MAX_VALUE;
 		double maxX = Double.MIN_VALUE, maxY = Double.MIN_VALUE;
 		
-		for(Vertex vertex : graph.vertexes)
+		for (Vertex vertex : graph.vertexes)
 		{
-			if(vertex.x.get() < minX)
-				minX = vertex.x.get();
-			
-			if(vertex.y.get() < minY)
-				minY = vertex.y.get();
-			
-			if(vertex.x.get() > maxX)
-				maxX = vertex.x.get();
-			
-			if(vertex.y.get() > maxY)
-				maxY = vertex.y.get();
+			minX = Math.min(minX, vertex.x.get());
+			maxX = Math.max(maxX, vertex.x.get());
+			minY = Math.min(minY, vertex.y.get());
+			maxY = Math.max(maxY, vertex.y.get());
+		}
+		
+		for (Edge edge : graph.edges)
+		{
+			if (!edge.isLinear())
+			{
+				Rectangle2D rect = edge.getArc().getBounds2D();
+				minX = Math.min(minX, rect.getMinX());
+				maxX = Math.max(maxX, rect.getMaxX());
+				minY = Math.min(minY, rect.getMinY());
+				maxY = Math.max(maxY, rect.getMaxY());
+			}
+		}
+		
+		for (Caption caption : graph.captions)
+		{
+			minX = Math.min(minX, caption.x.get());
+			maxX = Math.max(maxX, caption.x.get());
+			minY = Math.min(minY, caption.y.get());
+			maxY = Math.max(maxY, caption.y.get());
 		}
 		
 		return new Rectangle2D.Double(minX, minY, maxX - minX, maxY - minY);
