59fcb4f3273606297423dad9bc4a4558b15a73c0
0x24a537r9 <0x24a537r9@8559fdbf-7309-dc73-cc61-87a2367b2425>
2010-08-11 04:35:04 +0000

    Minor changes in ObservableCollection (removed unnecessary "name" property), and full javadoc documentation of the ObservableCollection class.  Also, minor inconsequential formatting changes to the javadoc documentation of the Graph class.

--- trunk/VisiGraph/src/edu/belmont/mth/visigraph/models/functions/FunctionService.java
+++ trunk/VisiGraph/src/edu/belmont/mth/visigraph/models/functions/FunctionService.java
@@ -10,24 +10,24 @@ import bsh.Interpreter;
 
 /**
  * @author Cameron Behar
- *
+ * 
  */
 public class FunctionService
-{	
-	public final static FunctionService instance = new FunctionService();
+{
+	public final static FunctionService instance = new FunctionService( );
 	
 	public final ObservableList<FunctionBase> functions;
 	
 	private FunctionService ( )
 	{
-		functions = new ObservableList<FunctionBase>("functions");
+		functions = new ObservableList<FunctionBase>( );
 		
-		functions.add(new CountCrossings());
+		functions.add( new CountCrossings( ) );
 		
-		File folder = new File("functions");
-		if(folder.exists())
-			for(String filename : folder.list( new FilenameFilter() { public boolean accept(File dir, String name) { return name.endsWith(".function"); } } ))
-				try { functions.add((FunctionBase)new Interpreter().source("functions/" + filename)); }
-				catch (Exception ex) { DebugUtilities.logException(String.format("An exception occurred while compiling %s.", filename), ex); }
+		File folder = new File( "functions" );
+		if ( folder.exists( ) )
+			for ( String filename : folder.list( new FilenameFilter( ) { public boolean accept( File dir, String name ) { return name.endsWith( ".function" ); } } ) )
+				try { functions.add( (FunctionBase) new Interpreter( ).source( "functions/" + filename ) );	}
+				catch ( Exception ex ) { DebugUtilities.logException( String.format( "An exception occurred while compiling %s.", filename ), ex ); }
 	}
 }
--- trunk/VisiGraph/src/edu/belmont/mth/visigraph/models/generators/GeneratorService.java
+++ trunk/VisiGraph/src/edu/belmont/mth/visigraph/models/generators/GeneratorService.java
@@ -10,32 +10,41 @@ import bsh.Interpreter;
 
 /**
  * @author Cameron Behar
- *
+ * 
  */
 public class GeneratorService
-{	
-public final static GeneratorService instance = new GeneratorService();
+{
+	public final static GeneratorService		instance	= new GeneratorService( );
 	
-	public final ObservableList<GeneratorBase> generators;
+	public final ObservableList<GeneratorBase>	generators;
 	
 	private GeneratorService ( )
 	{
-		generators = new ObservableList<GeneratorBase>("generators");
+		generators = new ObservableList<GeneratorBase>( );
 		
-		generators.add(new EmptyGraph());
+		generators.add( new EmptyGraph( ) );
 		
-		File folder = new File("generators");
-		if(folder.exists())
+		File folder = new File( "generators" );
+		if ( folder.exists( ) )
 		{
-			for(String filename : folder.list( new FilenameFilter() { public boolean accept(File dir, String name) { return name.endsWith(".generator"); } } ))
+			for ( String filename : folder.list( new FilenameFilter( )
+			{
+				public boolean accept( File dir, String name )
+				{
+					return name.endsWith( ".generator" );
+				}
+			} ) )
 			{
 				try
 				{
-					GeneratorBase generator = (GeneratorBase)new Interpreter().source("generators/" + filename);
-					ValidateGraphGenerator(generator);
-					generators.add(generator);
+					GeneratorBase generator = (GeneratorBase) new Interpreter( ).source( "generators/" + filename );
+					ValidateGraphGenerator( generator );
+					generators.add( generator );
 				}
-				catch (Throwable ex) { DebugUtilities.logException(String.format("An exception occurred while compiling %s.", filename), ex); }
+				catch ( Throwable ex )
+				{
+					DebugUtilities.logException( String.format( "An exception occurred while compiling %s.", filename ), ex );
+				}
 			}
 		}
 	}
@@ -43,8 +52,8 @@ public final static GeneratorService instance = new GeneratorService();
 	/**
 	 * Validates each {@link GeneratorBase} specified in {@link userSettings} to ensure logical consistency among its rules.
 	 * <p/>
-	 * This method does not return a value, but instead throws an {@link Exception} if an {@link GeneratorBase} implements inconsistent rules.
-	 * Because loops, multiple edges, and cycles are not mutually exclusive concepts, we must provide this sanity-check to ensure that no
+	 * This method does not return a value, but instead throws an {@link Exception} if an {@link GeneratorBase} implements inconsistent rules. Because
+	 * loops, multiple edges, and cycles are not mutually exclusive concepts, we must provide this sanity-check to ensure that no
 	 * {@link GeneratorBase} allows inconsistency or ambiguity. The logic used to validate internal consistency follows:
 	 * <p/>
 	 * <ul>
@@ -74,26 +83,25 @@ public final static GeneratorService instance = new GeneratorService();
 	 * </ul>
 	 * 
 	 * @author Cameron Behar
-	 * @throws Error
-	 *             If an {@link GeneratorBase} in the {@link userSettings}'s registry implements inconsistent rules.
+	 * @throws Error If an {@link GeneratorBase} in the {@link userSettings}'s registry implements inconsistent rules.
 	 */
-	private static void ValidateGraphGenerator (GeneratorBase generator) throws Error
+	private static void ValidateGraphGenerator( GeneratorBase generator ) throws Error
 	{
-		if (!generator.areCyclesAllowed().isTrue())
+		if ( !generator.areCyclesAllowed( ).isTrue( ) )
 		{
-			if (generator.areCyclesAllowed().isForced())
+			if ( generator.areCyclesAllowed( ).isForced( ) )
 			{
-				if (!(!generator.areLoopsAllowed().isTrue() && generator.areLoopsAllowed().isForced()))
-					throw new Error("The \"" + generator + "\" generator's default rules are inconsistent: if cycles are disallowed by force, loops must also be disallowed by force.");
-				if (!(!generator.areMultipleEdgesAllowed().isTrue() && generator.areMultipleEdgesAllowed().isForced()))
-					throw new Error("The \"" + generator + "\" generator's default rules are inconsistent: if cycles are disallowed by force, multiple edges must also be disallowed by force.");
+				if ( !( !generator.areLoopsAllowed( ).isTrue( ) && generator.areLoopsAllowed( ).isForced( ) ) )
+					throw new Error( "The \"" + generator + "\" generator's default rules are inconsistent: if cycles are disallowed by force, loops must also be disallowed by force." );
+				if ( !( !generator.areMultipleEdgesAllowed( ).isTrue( ) && generator.areMultipleEdgesAllowed( ).isForced( ) ) )
+					throw new Error( "The \"" + generator + "\" generator's default rules are inconsistent: if cycles are disallowed by force, multiple edges must also be disallowed by force." );
 			}
 			else
 			{
-				if (generator.areLoopsAllowed().isTrue())
-					throw new Error("The \"" + generator + "\" generator's default rules are inconsistent: if cycles are disallowed by default, loops must also be disallowed, either by force or by default.");
-				if (generator.areMultipleEdgesAllowed().isTrue())
-					throw new Error("The \"" + generator + "\" generator's default rules are inconsistent: if cycles are disallowed by default, multiple edges must also be disallowed, either by force or by default.");
+				if ( generator.areLoopsAllowed( ).isTrue( ) )
+					throw new Error( "The \"" + generator + "\" generator's default rules are inconsistent: if cycles are disallowed by default, loops must also be disallowed, either by force or by default." );
+				if ( generator.areMultipleEdgesAllowed( ).isTrue( ) )
+					throw new Error( "The \"" + generator + "\" generator's default rules are inconsistent: if cycles are disallowed by default, multiple edges must also be disallowed, either by force or by default." );
 			}
 		}
 	}
