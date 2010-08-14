7ef810defc0681a5c191f6bf3607bd4d00d4de2a
0x24a537r9 <0x24a537r9@8559fdbf-7309-dc73-cc61-87a2367b2425>
2010-08-14 23:43:01 +0000

    Added full javadoc documentation to the FunctionBase and GeneratorBase interfaces.

--- trunk/VisiGraph/src/edu/belmont/mth/visigraph/models/functions/FunctionBase.java
+++ trunk/VisiGraph/src/edu/belmont/mth/visigraph/models/functions/FunctionBase.java
@@ -7,14 +7,35 @@ import java.awt.*;
 import edu.belmont.mth.visigraph.models.*;
 
 /**
+ * The {@code FunctionBase} interface defines the minimal implementation requirements of graph-evaluating functions.
+ * 
  * @author Cameron Behar
- *
  */
 public interface FunctionBase
 {	
+	/**
+	 * Evaluates the function on the specified graph and returns its result as a {@code String}
+	 * 
+	 * @param g2D the {@code Graphics2D} context onto which the graph has been rendered
+	 * @param g the {@code Graph} on which to evaluate this {@code Function} 
+	 * 
+	 * @return a {@code String} representing this {@code Function}'s result
+	 */
 	public String evaluate(Graphics2D g2D, Graph g);
 	
+	/**
+	 * Returns a {@code boolean} indicating whether or not this function may be evaluated dynamically. For subclasses that modify the graph in any
+	 * way, or otherwise perform computationally expensive operations, it is advised that this method always return {@code false}.
+	 * 
+	 * @return {@code true} if dynamic evaluation is allowed, {@code false} otherwise
+	 */
 	public boolean allowsDynamicEvaluation();
 	
+	/**
+	 * Returns a {@code boolean} indicating whether or not this function may be evaluated statically. For subclasses designed to evaluate graphs
+	 * progressively over time it is advised that this method always return {@code true}.
+	 * 
+	 * @return {@code true} if one-time evaluation is allowed, {@code false} otherwise
+	 */
 	public boolean allowsOneTimeEvaluation();
 }
--- trunk/VisiGraph/src/edu/belmont/mth/visigraph/models/generators/GeneratorBase.java
+++ trunk/VisiGraph/src/edu/belmont/mth/visigraph/models/generators/GeneratorBase.java
@@ -1,42 +1,92 @@
 /**
- * GraphGeneratorBase.java
+ * GeneratorBase.java
  */
 package edu.belmont.mth.visigraph.models.generators;
 
 import edu.belmont.mth.visigraph.models.*;
 
 /**
+ * The {@code GeneratorBase} interface defines the minimal implementation requirements of graph-generating algorithms.
+ * 
  * @author Cameron Behar
- *
  */
 public interface GeneratorBase
-{	
-	public     BooleanRule areLoopsAllowed();
+{
+	/**
+	 * Returns whether or not loops should be allowed in the graph and whether or not this rule should be subject to change
+	 * 
+	 * @return a {@link BooleanRule} indicating both the rule and its enforcement policy
+	 */
+	public BooleanRule areLoopsAllowed( );
 	
-	public     BooleanRule areMultipleEdgesAllowed();
+	/**
+	 * Returns whether or not multi-edges should be allowed in the graph and whether or not this rule should be subject to change
+	 * 
+	 * @return a {@link BooleanRule} indicating both the rule and its enforcement policy
+	 */
+	public BooleanRule areMultipleEdgesAllowed( );
 	
-	public     BooleanRule areDirectedEdgesAllowed();
+	/**
+	 * Returns whether or not the graph should be directed and whether or not this rule should be subject to change
+	 * 
+	 * @return a {@link BooleanRule} indicating both the rule and its enforcement policy
+	 */
+	public BooleanRule areDirectedEdgesAllowed( );
 	
-	public     BooleanRule areCyclesAllowed();
+	/**
+	 * Returns whether or not cycles should be allowed in the graph and whether or not this rule should be subject to change
+	 * 
+	 * @return a {@link BooleanRule} indicating both the rule and its enforcement policy
+	 */
+	public BooleanRule areCyclesAllowed( );
 	
-	public     BooleanRule areParametersAllowed();
+	/**
+	 * Returns whether or not parameters should be allowed and whether or not this rule should be subject to change
+	 * 
+	 * @return a {@link BooleanRule} indicating both the rule and its enforcement policy
+	 */
+	public BooleanRule areParametersAllowed( );
 	
-	public     String      getParametersDescription();
+	/**
+	 * Returns a simple description of the input format for parameters
+	 * 
+	 * @return a {@code String} describing the input format for parameters
+	 */
+	public String getParametersDescription( );
 	
-	public     String	   getParametersValidatingExpression();
+	/**
+	 * Returns a regular expression used to validate parameter input formatting
+	 * 
+	 * @return the validating regular expression as a {@code String}
+	 */
+	public String getParametersValidatingExpression( );
 	
-	public     Graph       generate(String params, boolean areLoopsAllowed, boolean areDirectedEdgesAllowed, boolean areMultipleEdgesAllowed, boolean areCyclesAllowed);
-
+	/**
+	 * Generates a graph from the specified parameters and rules.
+	 * 
+	 * @param params a {@code String} of parameters for the generation algorithm
+	 * @param areLoopsAllowed a {@code boolean} indicating whether or not loops are to be allowed in the graph
+	 * @param areDirectedEdgesAllowed a {@code boolean} indicating whether the graph is to be directed or undirected
+	 * @param areMultipleEdgesAllowed a {@code boolean} indicating whether or not multi-edges are to be allowed in the graph
+	 * @param areCyclesAllowed a {@code boolean} indicating whether or not cycles are to be allowed in the graph
+	 * 
+	 * @return the newly generated {@code Graph} object
+	 */
+	public Graph generate( String params, boolean areLoopsAllowed, boolean areDirectedEdgesAllowed, boolean areMultipleEdgesAllowed, boolean areCyclesAllowed );
+	
+	/**
+	 * A boolean rule indicating whether an action should be allowed or prohibited, and whether or not this rule can be overridden
+	 */
 	public enum BooleanRule
 	{
 		ForcedTrue, DefaultTrue, ForcedFalse, DefaultFalse;
 		
-		public boolean isTrue()
+		public boolean isTrue( )
 		{
 			return this == ForcedTrue || this == DefaultTrue;
 		}
 		
-		public boolean isForced()
+		public boolean isForced( )
 		{
 			return this == ForcedTrue || this == ForcedFalse;
 		}
